0531c8625aaa230d298176e5dc2da0ac2fec3a5e
lesnikovskiy <lesnikovski@gmail.com>
2024-08-12 22:27:48 +0300

    create a function with lifetime error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+fn next_language(languages: &[String], current: &str) -> &str {
+    let mut found = false;
+
+    for language in languages {
+        if found {
+            return language;
+        }
+
+        if language == current {
+            found = true;
+        }
+    }
+
+    languages.last().unwrap()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let languages = vec![String::from("rust"), String::from("go"), String::from("typescript")];
+
+    let result = next_language(&languages, "go");
+    println!("{}", result);
 }
