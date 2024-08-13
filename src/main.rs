1438373e90ecba5b8141238b3627a73db4ff18f6
lesnikovskiy <lesnikovski@gmail.com>
2024-08-13 22:25:19 +0300

    implement my own method with lifetime annotation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn next_language(languages: &[String], current: &str) -> &str {
+fn next_language<'a>(languages: &'a [String], current: &str) -> &'a str {
     let mut found = false;
 
     for language in languages {
@@ -14,9 +14,25 @@ fn next_language(languages: &[String], current: &str) -> &str {
     languages.last().unwrap()
 }
 
+fn find_keyboard<'a>(keyboards: &'a [String], search_term: &str) -> Option<&'a String> {
+    keyboards.iter().find(|k| k.contains(&search_term))
+}
+
 fn main() {
     let languages = vec![String::from("rust"), String::from("go"), String::from("typescript")];
 
     let result = next_language(&languages, "go");
     println!("{}", result);
+
+    let keyboards = vec![
+        String::from("Yamaha"),
+        String::from("Roland"),
+        String::from("Casio"),
+        String::from("Moog")
+    ];
+
+    match find_keyboard(&keyboards, "Ya") {
+        Some(keyboard) => println!("Keyboard found: {}", keyboard),
+        None => println!("No keyboard found"),
+    }
 }
