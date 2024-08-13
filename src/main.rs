6cc8cbd89096a1fe655da0a3a9243b45affff9c5
lesnikovskiy <lesnikovski@gmail.com>
2024-08-13 22:46:41 +0300

    longest common example but not the best one

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,10 @@ fn last_language(languages: &[String]) -> &str {
     languages.last().unwrap()
 }
 
+fn longest_language<'a>(lang1: &'a str, lang2: &'a str) -> &'a str {
+    if lang1.len() > lang2.len() { lang1 } else { lang2 }
+}
+
 fn main() {
     let languages = vec![String::from("rust"), String::from("go"), String::from("typescript")];
 
@@ -26,4 +30,7 @@ fn main() {
 
     let last_lang = last_language(&languages);
     println!("Last language: {}", last_lang);
+
+    let longest = longest_language("go", "typescript");
+    println!("The longest language is {}", longest);
 }
